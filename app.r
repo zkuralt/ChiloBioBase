662e16e2aa30134fe352a292438108345e9ecd75
Zan Kuralt <zan.kuralt@gmail.com>
2017-09-26 22:59:28 +0200

    Working on app UI, and adding some comments

--- app.r
+++ app.r
@@ -1,18 +1,19 @@
+# Load needed packages
 library(shiny)
 library(shinydashboard)
 
+# Formatting header. Need to look into the dropdown menus.
 header <- dashboardHeader(title = "ChiloBioBase 2.0",
                           dropdownMenu(type = "notifications",
                                        notificationItem(
                                          text = "4 records added today",
-                                         icon("exclamation-triangle")
-                                       )
-                          ),
+                                         icon("exclamation-triangle"))),
                           dropdownMenu(type = "tasks",
                                        taskItem(value = 0, color = "red", "Importing existing flatfile database"),
                                        taskItem(value = 4, color = "blue", "Setting up the database"),
                                        taskItem(value = 100, color = "green", "Records waiting for confirmation")))
 
+# Formatting sidebar. What would be the most practical structure of it?
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("About database", tabName = "about", icon = icon("database")),
@@ -23,13 +24,14 @@ sidebar <- dashboardSidebar(
   )
 )
 
+# Formatting page body. Not quite there yet ...
 body <- dashboardBody()
 
 ui <- dashboardPage(header, sidebar, body, skin = "black")
 
 
 server <- function(input, output) {
-  
+# Huh, server needs some work too :) 
 }
 
   shinyApp(ui, server)
