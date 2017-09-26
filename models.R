53d38d815667fc7919c2a64e98b7d075d4206461
zankuralt <zan.kuralt@gmail.com>
2017-09-26 14:39:27 +0200

    updated models

--- models.R
+++ models.R
@@ -1,22 +1,23 @@
 dbSendQuery(conn = con, statement = 
-              "CREATE TABLE IF NOT EXISTS Users(
-            user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            Name VARCHAR(40),
-            Surname VARCHAR(40),
-            Email VARCHAR(50),
-            Role VARCHAR(20)
+              "CREATE TABLE IF NOT EXISTS Roles(
+            Role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            Role VARCHAR(20),
+            Privileges VARCHAR(30)
             )")
 
 dbSendQuery(conn = con, statement = 
-              "CREATE TABLE IF NOT EXISTS Roles(
-            role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            Role VARCHAR(20),
-            Privileges VARCHAR(50)
+              "CREATE TABLE IF NOT EXISTS Users(
+            User_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            Role_id INT,
+            Name VARCHAR(40),
+            Surname VARCHAR(40),
+            Email VARCHAR(50),
+            FOREIGN KEY (Role_id) REFERENCES Roles(Role_id)
             )")
 
 dbSendQuery(conn = con, statement =  
               "CREATE TABLE IF NOT EXISTS Species(
-            spec_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            Species_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
             Genus VARCHAR(20),
             Subgenus VARCHAR(20),
             Species VARCHAR(20),
@@ -33,7 +34,7 @@ dbSendQuery(conn = con, statement =
 
 dbSendQuery(conn = con, statement = 
               "CREATE TABLE IF NOT EXISTS Locality(
-            loc_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            Locality_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
             Locality_Name VARCHAR(30),
             Locality_PlaceNear VARCHAR(30),
             Locality_OtherNames VARCHAR(30),
@@ -53,13 +54,60 @@ dbSendQuery(conn = con, statement =
 
 dbSendQuery(conn = con, statement = 
               "CREATE TABLE IF NOT EXISTS Survey(
-            survey_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            Survey_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
             Date DATE,
             Sampling_unit VARCHAR(20),
-            Collector VARCHAR(20)
+            Collected_by VARCHAR(20)
             )")
 
+dbSendQuery(conn = con, statement = 
+              "CREATE TABLE IF NOT EXISTS Collection(
+            Collection_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            Collection VARCHAR(20),
+            Inventary_Number VARCHAR(20),
+            Specimen_Type VARCHAR(10),
+            Determined_by VARCHAR(10)
+            )")
 
+dbSendQuery(conn = con, statement =
+              "CREATE TABLE IF NOT EXISTS Morphological_data(
+            Morpho_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            Geophilomorpha_leg_count TINYINT,
+            Lithobiomorpha_leg_count TINYINT,
+            Scolopendromorpha_leg_count TINYINT,
+            Scutigeromorpha_leg_count TINYINT
+            )")
+
+dbSendQuery(conn = con, statement =
+              "CREATE TABLE IF NOT EXISTS Molecular_data(
+            Molecular_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            Marker_Type VARCHAR(20),
+            Sample_Type VARCHAR(20),
+            Sequencing_Type VARCHAR(20),
+            Sequence_Length VARCHAR(5),
+            Sequence TEXT
+            )")
+
+dbSendQuery(conn = con, statement = 
+              "CREATE TABLE IF NOT EXISTS Specimen(
+            Specimen_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            Species_id INT,
+            FOREIGN KEY(Species_id) REFERENCES Species(Species_id),
+            Sex VARCHAR(10),
+            Stage VARCHAR(10),
+            Locality_id INT,
+            FOREIGN KEY(Locality_id) REFERENCES Locality(Locality_id),
+            Survey_id INT,
+            FOREIGN KEY(Survey_id) REFERENCES Survey(Survey_id),
+            Collection_id,
+            FOREIGN KEY(Collection_id) REFERENCES Collection(Collection_id),
+            Morpho_id,
+            FOREIGN KEY(Morpho_id) REFERENCES Morphological_data(Morpho_id)
+            Molecular_id,
+            FOREIGN KEY(Molecular_id) REFERENCES Molecular_data(Molecular_id)
+            Added_by
+            FOREIGN KEY(Added_by) REFERENCES Users(user_id),
+            )")
 
 
 
